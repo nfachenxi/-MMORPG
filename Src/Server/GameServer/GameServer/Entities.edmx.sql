73eba338d7b2154b800633483ff534b047c78e73
nfachenxi <nfachenxi@163.com>
2024-09-28 23:50:23 +0800

    数据库相关内容测试与修正

--- Src/Server/GameServer/GameServer/Entities.edmx.sql
+++ Src/Server/GameServer/GameServer/Entities.edmx.sql
@@ -2,7 +2,7 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 09/26/2024 23:45:49
+-- Date Created: 09/28/2024 23:45:16
 -- Generated from EDMX file: F:\MMORPG\mymmo\Src\Server\GameServer\GameServer\Entities.edmx
 -- --------------------------------------------------
 
